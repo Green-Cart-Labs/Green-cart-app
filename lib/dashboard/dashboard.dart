0fa66aded4e71b54e2192112b848bba178883ca4
ishar19 <isharjain19@gmail.com>
2024-01-26 21:57:46 +0530

    feat:added route to loign

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,36 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class Dashboard extends StatefulWidget {
-  const Dashboard({super.key});
+  const Dashboard({Key? key}) : super(key: key);
 
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  final String assetName = 'assets/images/logo.svg';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                SvgPicture.asset(assetName),
-                const SizedBox(width: 10,),
-                Text(
-                  "BLOOM",
-                  style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w900),
-                ),
-              ],
-            ),
-          ],
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF909E47), Color(0xFFF6F7ED), Color(0xFF909E47)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("BLOOM",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  context.go('/login');
+                  // Add your logic here for the button press
+                  // You can navigate to another screen, perform an action, etc.
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
