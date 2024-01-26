a52a99bf3a335113f442099a6c7e982af6deb097
janviguliyan <JANVIGULIYAN@GMAIL.COM>
2024-01-26 13:11:10 +0530

    added theme files

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final themeData = ThemeData(
+  scaffoldBackgroundColor: const Color(0xFFF6F7ED),
+  // scaffoldBackgroundColor: Colors.transparent,
+  primaryColor: const Color(0xFF295B3E),
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF909E47)),
+  useMaterial3: true,
+);
