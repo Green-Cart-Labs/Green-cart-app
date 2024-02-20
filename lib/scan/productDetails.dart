b3fd8672fd76037e90fab774e7c80c8808271cbb
ishar19 <isharjain19@gmail.com>
2024-02-20 09:08:13 +0530

    refactor: added user id creation and in scan request (#21)

--- lib/scan/productDetails.dart
+++ lib/scan/productDetails.dart
@@ -7,7 +7,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:green_cart/widgets/gradient_container.dart';
 import 'package:green_cart/api_service.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:firebase_auth/firebase_auth.dart';
 import '../chat.dart';
 import '../constants.dart';
 import '../models/barcodeDetails.dart';
@@ -21,11 +21,6 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
-  // late List<BarcodeDetails>? _barcodeDetails =[];
-  // BarcodeDetails barcodeDetails ;
-  // late String _barcodeDetails = '';
-  // int? barcode = int.tryParse(widget.child);
-  // late Future<String> _futureBarcodeDetails;
   late Future<BarcodeDetails>? _futureBarcodeDetails;
   // late Future<Nutri>? _futureNutriDetails;
 
@@ -50,11 +45,11 @@ class _ProductDetailsState extends State<ProductDetails> {
         log('response statuscode:: ${jsonDecode(response.body)}');
         log('message : ${BarcodeDetails.fromJson(jsonDecode(response.body)).imageUrl}');
         // setState(() {
-          // _jsonData = jsonDecode(response.body);
-          // _jsonData = BarcodeDetails.fromJson(response.body);
+        // _jsonData = jsonDecode(response.body);
+        // _jsonData = BarcodeDetails.fromJson(response.body);
         // });
         // return response.body;
-        return BarcodeDetails.fromJson(jsonDecode(response.body) );
+        return BarcodeDetails.fromJson(jsonDecode(response.body));
       } else {
         log('Failed to load barcode details: ${response.statusCode}');
         // throw Exception('Failed to create album.');
@@ -64,6 +59,14 @@ class _ProductDetailsState extends State<ProductDetails> {
     }
     log('barcode n userid: $barcode, $user_id');
     throw Exception('Failed.');
+  }
+
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  String? getUserId() {
+    final User? user = auth.currentUser;
+    final String? uid = user?.uid.toString();
+    return uid;
   }
 
   // Future<Nutri> getNutri(int barcode, String user_id) async {
@@ -104,10 +107,11 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   void initState() {
     super.initState();
-      _futureBarcodeDetails = getBarcode(widget.child, '0');
-      // _futureNutriDetails = getNutri(widget.child, '0');
+    _futureBarcodeDetails = getBarcode(widget.child, getUserId() ?? '0');
 
   }
+
+  @override
 
   // void getBarcodeDetails(int barcode, String user_id) async {
   //   // _barcodeDetails = (await ApiService().getBarcode(barcode, user_id))!;
@@ -119,13 +123,13 @@ class _ProductDetailsState extends State<ProductDetails> {
   //   });
   //   });
 
-    // ApiService apiService = ApiService();
-    // try {
-      // String? response = await apiService.getBarcode(barcode, user_id);
-      // print('*** : $response');
-    // } catch (e) {
-    //   print('Error: $e');
-    // }
+  // ApiService apiService = ApiService();
+  // try {
+  // String? response = await apiService.getBarcode(barcode, user_id);
+  // print('*** : $response');
+  // } catch (e) {
+  //   print('Error: $e');
+  // }
   // }
 
   @override
@@ -221,6 +225,7 @@ class _ProductDetailsState extends State<ProductDetails> {
       floatingActionButton: ChatButton(),
       body: GradientContainer(
         child: Padding(
+
           padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0, top: 30.0),
           child: FutureBuilder<BarcodeDetails?>(
             future: _futureBarcodeDetails,
@@ -263,12 +268,48 @@ class _ProductDetailsState extends State<ProductDetails> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Row(
+                          children: [
+                            Icon(
+                              Icons.arrow_back_ios,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            Text(
+                              "Go back",
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Center(child: CircularProgressIndicator()),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
                           onPressed: () {
                             Navigator.pop(context);
                           },
                           icon: Row(
                             children: [
-                              Icon(Icons.arrow_back_ios,color: Theme.of(context).primaryColor,),
+                              Icon(
+                                Icons.arrow_back_ios,
+                                color: Theme.of(context).primaryColor,
+                              ),
                               Text(
                                 "Go back",
                                 style: TextStyle(
@@ -279,16 +320,8 @@ class _ProductDetailsState extends State<ProductDetails> {
                               ),
                             ],
                           ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        "Product Name: ${snapshot.data!.name.toString()}",
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
                         ),
+
                       ),
                       Text("${snapshot.data!.brands ?? 'No data' }", style: TextStyle(fontSize: 18),),
                       // const SizedBox(
@@ -300,30 +333,17 @@ class _ProductDetailsState extends State<ProductDetails> {
                           fontSize: 18,
                           // fontWeight: FontWeight.bold,
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Center(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.network(
-                            'https:${snapshot.data!.imageUrl.toString()}',
-                            // 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png',
-                            fit: BoxFit.fill,
+                        Text(
+                          "Product Name: ${snapshot.data!.name.toString()}",
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
                           ),
-                          // SvgPicture.asset(assetName, fit: BoxFit.contain,),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        "Product Details:",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
+                        const SizedBox(
+                          height: 10,
                         ),
+
                       ),
                       Card(
                         color: getColorFromData(snapshot.data?.ecoscoreGrade),
